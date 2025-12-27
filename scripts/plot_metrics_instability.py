8b60219aece7bcfc2e65c821442f5d71b580734f
Pavankumar Suresh <Pavancseds@gmail.com>
2025-12-26 23:49:55 -0800

    Metric instability script fixed

--- scripts/plot_metrics_instability.py
+++ scripts/plot_metrics_instability.py
@@ -1,14 +1,28 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("/Users/pavankumar_s/Desktop/Data Engineering/real_time_tickets_observability/dbt_project/dbs_obs/metric_instability.csv")
+df = pd.read_csv(
+    "/Users/pavankumar_s/Desktop/Data Engineering/real_time_tickets_observability/dbt_project/dbs_obs/data/exports/metric_instability.csv"
+)
 
-plt.figure(figsize=(8,5))
+plt.figure(figsize=(8, 5))
 plt.plot(df["checkpoint"], df["tickets_sold"], marker="o")
-plt.title("Metric Instability Due to Late Arriving Data")
-plt.xlabel("Observation Time")
-plt.ylabel("Tickets Sold")
+
+for i, row in df.iterrows():
+    plt.text(
+        row["checkpoint"],
+        row["tickets_sold"],
+        row["observed_at"].split(".")[0],  
+        ha="right",
+        va="bottom",
+        fontsize=8
+    )
+
+plt.title("Metric Instability Observed for December 20,2025 due to Late-Arrival of Data")
+plt.xlabel("When metrics were queried")
+plt.ylabel("Tickets sold")
 plt.grid(True)
 
 plt.tight_layout()
+plt.savefig("metric_instability.png", dpi=150)
 plt.show()
